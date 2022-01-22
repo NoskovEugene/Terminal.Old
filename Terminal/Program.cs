4a8a10f470194c79faca3c91b9c8372a6fa1c83c
Eugene Noskov <NoskovEugene@gmail.com>
2022-01-22 21:40:31 +0300

    Create models projects

--- Terminal/Program.cs
+++ Terminal/Program.cs
@@ -1,9 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System;
+
 namespace Terminal;
 public static class Program
 {
     public static void Main(string[] args)
     {
+        
     }
 }
